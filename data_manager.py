02fe85ab83c68620134bc1d3a8c587b1b6309cc1
Emil <emil.karlstrom@gmail.com>
2020-05-06 23:18:10 +0200

    Visualization

--- data_manager.py
+++ data_manager.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+
 import datetime
 import time
 CSV_SEPARATOR = ';'
@@ -19,26 +19,26 @@ def load_data(data):
     path = f'{WEATHER_DATA_PATH}/{WEATHER_PAIRS[data]}'
     df = pd.read_csv(path, sep=CSV_SEPARATOR)
     
-    df['Year'], df['Month'], df['Day'] = df['Datum'].str.split('-').str
+    df[['Year', 'Month', 'Day']] = df['Datum'].str.split('-', expand=True)
 
     # Create a bunch of new columns with appropriate data and apply a function that turns the data into integer formats
-    df['Year']  = df['Year'].map(lambda a: int(a.split(':')[0]))
-    df['Month'] = df['Month'].map(lambda a: int(a.split(':')[0]))
-    df['Day']   = df['Day'].map(lambda a: int(a.split(':')[0]))
+    df['Year']  = df['Year'].map(lambda a: int(a))
+    df['Month'] = df['Month'].map(lambda a: int(a))
+    df['Day']   = df['Day'].map(lambda a: int(a))
     df['Hour'] = df['Tid (UTC)'].map(lambda a: int(a.split(':')[0]))
     
     # Advanced line of code that takes all the columns Year, Month, Day, and Hour and aggregates them into one single separate column called "Whole"
     # It turns the Year, Month, Day, and Hour data into a timestamp using the datetime library for easy comparison between different times
     # Making finding interval data very simple
     df['Whole'] = df[['Year', 'Month', 'Day', 'Hour']].agg(lambda a: datetime.datetime(*a).timestamp(), axis=1)
-
+    df['Date']  = df[['Year', 'Month', 'Day', 'Hour']].agg(lambda a: ':'.join([str(v) for v in a]), axis=1)
     # Drop the labels we aren't going to use
     df = df.drop(labels=['Tid (UTC)', 'Datum'], axis=1)
     df = df.rename(columns={df.columns[0]: 'Value'})
 
     # Reorganize the labels, the first label in the dataframe with dropped labels will be the "value" we are looking for
     # This is most likely a temporary step as it won't matter later on when we visualize the data as a graph
-    df = df[['Year', 'Month', 'Day', 'Hour', 'Whole', df.columns[0]]]
+    df = df[['Year', 'Month', 'Day', 'Hour', 'Date', 'Whole', df.columns[0]]]
     return df
 
 def get_interval(df: pd.DataFrame, min_time: datetime.datetime, max_time: datetime.datetime):
@@ -53,9 +53,3 @@ def get_interval(df: pd.DataFrame, min_time: datetime.datetime, max_time: dateti
     m = df.loc[(df['Whole'] >= min_time.timestamp()) & (df['Whole'] <= max_time.timestamp())]  #
     m = m.reset_index(drop=True)
     return m
-
-df = load_data('airtemp')
-min_date, max_date = datetime.datetime(2019, 12, 28), datetime.datetime(2019, 12, 28, 23)
-
-df = get_interval(df, min_date, max_date)
-print(df)
--- pages.py
+++ pages.py
@@ -1,4 +1,17 @@
+import matplotlib
+matplotlib.use('TkAgg')
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from matplotlib.figure import Figure
+from matplotlib import animation as animation
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
+
 import tkinter as tk
+import data_manager
+import datetime
+import math
 
 BIG_FONT = ('Helvetica', 18)
 
@@ -13,18 +26,62 @@ class Page(tk.Frame):
         super().__init__(master=parent)
         label = tk.Label(self, text='Visualizing data', font=BIG_FONT).pack()
         self.pack()
+        
+        #########################################
+        # Menu related things
 
         menu = tk.Menu(parent)
         controller.config(menu=menu)
 
+        # File related
         _file = tk.Menu(menu)
         _file.add_command(label='Exit', command=controller.client_exit)
         menu.add_cascade(label='File', menu=_file)
 
+        # Resize the window buttons
         _resize = tk.Menu(menu)
         for size in WINDOW_SIZES:
             _resize.add_command(label=size, command=self.ResizeCommand(controller, size))
         menu.add_cascade(label='Resize', menu=_resize)
+
+        # Attribute related
+
+        _attributes = tk.Menu(menu)
+        for data_name in data_manager.WEATHER_PAIRS:
+            _attributes.add_command(label=data_name, command=self.LoadDataCommand(self, data_name))
+        menu.add_cascade(label='Attributes', menu=_attributes)
+
+        self._figure = Figure(figsize=(5, 5), dpi=100)
+        self._plot = self._figure.add_subplot(111)
+
+        self._graph_canvas = FigureCanvasTkAgg(self._figure, self)
+        self._graph_canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+
+        self._toolbar = NavigationToolbar2Tk(self._graph_canvas, self)
+        self._toolbar.update()
+        self._graph_canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+    def animate(self, i):
+        pass
+    def plot_data(self, dataframe, label, color='red'):
+
+        count = dataframe.shape[0]
+        self._plot.set_title(f'Plotting {label}')
+        
+        self._plot.clear()
+        self._plot.set_xlabel('Time')
+        self._plot.set_xticks([])
+        self._plot.set_ylabel('Value')
+        self._plot.plot(range(count), dataframe['Value'], label=label, color=color)
+        self._plot.legend()
+
+        self._graph_canvas.draw()
+
+    def load_data(self, data_name, min_date=None, max_date=None):
+        data = data_manager.load_data(data_name)
+        if min_date or max_date:
+            data = data_manager.get_interval(data, min_date, max_date)
+        return data
 
     class ResizeCommand:
         def __init__(self, controller, size):
@@ -33,5 +90,13 @@ class Page(tk.Frame):
         def __call__(self):
             self.controller.set_size(self.size)
 
+    class LoadDataCommand:
+        def __init__(self, frame, data_name):
+            self.frame = frame
+            self.data_name = data_name
+        def __call__(self):
+            df = self.frame.load_data(self.data_name)
+            self.frame.plot_data(df, self.data_name)
+            
 START_FRAME = Page
 PAGES = (Page, )
